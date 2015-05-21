e88eb5dc4f8a192772062ac4fd879291eabaafe2
David Cormier <david.cormier@savoirfairelinux.com>
2015-05-21 14:15:01 -0400

    entry url: specify title
    Use the entry name as the title for the link.

--- js/jquery.fn.gantt.js
+++ js/jquery.fn.gantt.js
@@ -328,7 +328,7 @@
                     if (i >= element.pageNum * settings.itemsPerPage && i < (element.pageNum * settings.itemsPerPage + settings.itemsPerPage)) {
                         entries.push('<div class="row name row' + i + (entry.desc ? '' : ' fn-wide') + '" id="rowheader' + i + '" offset="' + i % settings.itemsPerPage * tools.getCellSize() + '">');
                         if ("url" in entry) {
-                            entries.push('<span class="fn-label' + (entry.cssClass ? ' ' + entry.cssClass : '') + '">' + '<a href="' + entry.url + '">' + (entry.name || '') + '</a>' + '</span>');
+                            entries.push('<span class="fn-label' + (entry.cssClass ? ' ' + entry.cssClass : '') + '">' + '<a href="' + entry.url + '" title="' + entry.name + '">' + (entry.name || '') + '</a>' + '</span>');
                         } else {
                             entries.push('<span class="fn-label' + (entry.cssClass ? ' ' + entry.cssClass : '') + '">' + (entry.name || '') + '</span>');
                         }
